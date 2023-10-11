52b9ca1db972ecbf62095fa0b536cc8880da7203
John Joseph Hilario <>
2023-10-12 02:07:47 +0800

    web: user and company routes

---
+++ apps/web/app/api/companyRoute.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+const apiUrl = process.env.API_URL || 'http://localhost:3000/api';
+
+export const getCompanies = async () => {
+  const endpoint = `${apiUrl}/companies`;
+
+  try {
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postCompany = async (
+  name: string,
+  address: string,
+  userId: [string]
+) => {
+  const endpoint = `${apiUrl}/companies`;
+
+  try {
+    const response = await axios.post(endpoint, { name, address, userId });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteCompany = async (id: string) => {
+  const endpoint = `${apiUrl}/companies/${id}`;
+
+  try {
+    const response = await axios.delete(endpoint);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
